f073dde7dd28ca5b02c994e301c4f607797bdb0a
leooso-max <leocastaoso@gmail.com>
2025-08-29 08:30:54 -0600

    Update merge_sort.c

--- merge_sort.c
+++ merge_sort.c
@@ -59,10 +59,58 @@ int main() {
 
 // ---- Implementa aquí tu función mergeSort ----
 void mergeSort(int arr[], int left, int right) {
-    // TODO
+    if (left < right) {
+        int mid = (left+right) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid+1, right);
+
+        merge(arr, left, mid, right);
+    }
 }
 
 // ---- Implementa aquí tu función merge ----
 void merge(int arr[], int left, int mid, int right) {
-    // TODO
+    int n1 = mid - left+1;
+    int n2 = right - mid;
+
+    int *L = (int *)malloc(n1 * sizeof(int));
+    int *R = (int *)malloc(n2 * sizeof(int));
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+        
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid+1 + j];
+
+    int i, j, k;
+
+    i = 0;
+    j = 0;
+    k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            k ++;
+            i ++;
+        } else {
+            arr[k] = R[j];
+            k ++;
+            j ++;
+        }
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++; 
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++; 
+    }
+
+    free(L);
+    free(R);
 }
